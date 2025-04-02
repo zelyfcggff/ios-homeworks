a618cf2324ab709d8e27f33e6645fda8b1cdcd7a
zelyfcggf <renegat2004@icloud.com>
2025-04-02 12:25:52 +0300

    Добавлен PostViewController и переход на него из FeedViewController через UINavigationController

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -1,4 +1,3 @@
-
 import UIKit
 
 class FeedViewController: UIViewController {
@@ -6,5 +5,24 @@ class FeedViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Лента"
+        setupButton()
+    }
+
+    private func setupButton() {
+        let button = UIButton(type: .system)
+        button.setTitle("Открыть пост", for: .normal)
+        button.addTarget(self, action: #selector(openPost), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+    @objc private func openPost() {
+        let postVC = PostViewController()
+        navigationController?.pushViewController(postVC, animated: true)
     }
 }
---
+++ Navigation/PostViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class PostViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+        title = "Пост"
+    }
+}
