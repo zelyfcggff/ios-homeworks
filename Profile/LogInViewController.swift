d60b8965981a78b7ea460cbc3053ec1e87c30271
zelyfcggf <renegat2004@icloud.com>
2025-04-08 11:47:19 +0300

    Добавил LogInViewController, ColorSet для VK и заменил экран Profile на экран входа

---
+++ Profile/LogInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LogInViewController.swift
+//  Navigation
+//
+//  Created by Vladimir Zykov on 08.04.2025.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
